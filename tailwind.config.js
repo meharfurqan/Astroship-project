5acb61549b9bd17b6e0426afe8c52c97d22aec01
meahrfurqan <meharfu8@gmail.com>
2024-08-07 17:17:10 +0500

    deploying project on github

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    theme: {
+      extend: {
+        fontFamily: {
+          customFont: [
+            "Bricolage Grotesque Variable", Inter, ui-sans-serif, system-ui, sans-serif,
+            "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji" 
+          ],
+        },
+      },
+    },
+  };
+  
